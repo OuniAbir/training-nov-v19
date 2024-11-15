374e14d187128ded8cae4b06de3a9973d52c6ab7
Yann-Thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2024-11-15 14:51:39 +0100

    Balise image

--- src/app/post/post-detail/post-detail.component.ts
+++ src/app/post/post-detail/post-detail.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject, input, SkipSelf } from '@angular/core';
 import { Post } from '../services/post.service';
 import { LoggerService } from 'lib';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.css',
+  imports: [NgOptimizedImage],
 })
 export class PostDetailComponent {
   constructor() {
